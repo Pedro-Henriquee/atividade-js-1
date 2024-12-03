51aad217625939e86a1cd6f12bb4f1732fb112e2
Pedro Henrique <pedrohenrique20311@gmail.com>
2024-12-03 08:52:00 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let h1 = document.getElementById("titulo")
+h1.innerText = "Conheça mais sobre a Proz Educação!"
+
+let a = document.querySelector("a")
+a.innerText = "Site da Proz"
+
+let ul = document.querySelector("ul")
+ul.innerHTML = `<li>A Proz Educação é uma empresa de educação que oferece cursos técnicos e profissionalizantes, com foco na experiência prática e enriquecedora dos alunos.</li>
+                <li>A Proz Educação tem salas e laboratórios equipados para proporcionar uma aprendizagem rica e objetiva.</li>
+                <li>A Proz Educação tem mais de 24 unidades em São Paulo, Minas Gerais e Paraná, e já formou mais de 40 mil alunos.</li>`
+
+let ol = document.getElementById("lista-ordenada")
+ol.innerHTML = `<li><a href="https://www.youtube.com/c/ProzEduca%C3%A7%C3%A3obr">Youtube - Proz</a></li>
+                <li><a href="https://www.instagram.com/prozeducacaobr/">Instagram - Proz</a></li>
+                <li><a href="https://www.facebook.com/prozeducacaobr/">Facebook - Proz</a></li>`
